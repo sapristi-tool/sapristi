f3bcf4aabecb39534e3159c8906e516e0adf5934
user.name <user.email>
2020-11-15 11:38:39 +0100

    Allow percentages for dimensition and position in configuration file

--- lib/sapristi/configuration_loader.rb
+++ lib/sapristi/configuration_loader.rb
@@ -6,7 +6,7 @@ module Sapristi
 		def initialize
 			@monitor_manager = MonitorManager.new
 		end
-		
+
 		def load file
 			table = CSV.read(file, headers: true, col_sep: SEPARATOR)
 			raise Error, "Invalid configuration file: headers=#{table.headers.join(', ')}, valid=#{valid_headers.join(', ')}" if !table.headers.eql? valid_headers
@@ -31,21 +31,18 @@ module Sapristi
 			translations = { "H-size" => "x", "V-size" => "y", "X-position" => "x", "Y-position" => "y" }
 			normalized = definition.to_h.keys.each_with_object({}) do |k, memo|
 				m = definition[k]&.to_s&.match(/^([0-9]{1,2})%$/)
-				m2 = definition[k]&.to_s&.match(/^([0-9]+)$/)
 				if m
 					if translations[k]
 						memo[k] = (monitor[translations[k]] * (m[1].to_i / 100.0)).to_i
 					else
 						raise "#{k}=#{definition[k]}, using percentage in invalid field, valid=#{translations.keys.join(', ')}"
 					end
-				elsif m2
-					memo[k] = definition[k].to_i
 				else
 					memo[k] = definition[k]
 				end
 			end
 
-			translations.keys.each {|k| normalized[k] = normalized[k].to_i }
+			(translations.keys + %w(Workspace)).each {|k| normalized[k] = normalized[k].to_i }
 			normalized
 		end
 	end
--- spec/configuration_loader_spec.rb
+++ spec/configuration_loader_spec.rb
@@ -36,7 +36,8 @@ RSpec.describe ConfigurationLoader do
 	let(:valid_csv_definitions) do
 		[
 			{"Title" => nil, "Command" => "some", "Monitor" => nil, "X-position" => 1, "Y-position" => 2, "H-size" => 3, "V-size" => 4, "Workspace" => 5, "Line" => 0},
-			{"Title" => "some title", "Command" => nil, "Monitor" => 6, "X-position" => 7, "Y-position" => 8, "H-size" => 9, "V-size" => 10, "Workspace" => 11, "Line" => 1}
+			{"Title" => "some title", "Command" => nil, "Monitor" => 6, "X-position" => 7, "Y-position" => 8, "H-size" => 9, "V-size" => 10, "Workspace" => 11, "Line" => 1},
+			{"Title" => "some title", "Command" => nil, "Monitor" => 0, "X-position" => "10%", "Y-position" => "20%", "H-size" => "30%", "V-size" => "40%", "Workspace" => 11, "Line" => 1}
 			]
 	end
 
@@ -49,7 +50,38 @@ RSpec.describe ConfigurationLoader do
 		expect { under_test.load(invalid_csv) }.to raise_error(Error, /Invalid configuration file: headers/)
 	end
 
-	it 'reads valid configuration file' do
-		expect(under_test.load(valid_csv)).to eql(valid_csv_definitions)
+	context 'configuration file' do
+		let(:content) { under_test.load(valid_csv) }
+
+		let(:xrandr_example) {
+%Q{Monitors: 2
+ 0: +*some 3840/597x2160/336+0+0  DP-1
+ 1: +another 1920/509x1080/286+3840+0  HDMI-1
+}
+	}
+
+		it 'line field is added' do
+			expect(content[0]["Line"]).to eql(0)
+		end
+
+		it 'numeric fields are integers' do
+			%w(Line X-position Y-position H-size V-size Workspace).each do |field|
+				expect(content[1][field]).not_to be_nil
+				expect(content[1][field]).to be_instance_of Integer
+			end
+		end
+
+		it 'apply percentage using monitor dimensions position fields' do
+			translations = { "H-size" => "x", "V-size" => "y", "X-position" => "x", "Y-position" => "y" }
+
+			
+
+			%w(X-position Y-position H-size V-size).each do |field|
+				monitor = { id: 0, name: "some", main: "*", x: 3840, y: 2160, offset_x: 0, offset_y: 0 }.transform_keys(&:to_s)
+				allow_any_instance_of(MonitorManager).to receive(:list_monitors).and_return(xrandr_example)
+
+				expect(content[2][field]).to be ( (valid_csv_definitions[2][field][0..-2].to_i / 100.0) * monitor[translations[field]]).to_i
+			end
+		end
 	end
 end
