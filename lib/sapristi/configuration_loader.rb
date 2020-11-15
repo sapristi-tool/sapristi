70a5b14b6c41b6e2075903d5e4ff519e3b5381c0
user.name <user.email>
2020-11-15 02:42:08 +0100

    Read csv content

--- lib/sapristi/configuration_loader.rb
+++ lib/sapristi/configuration_loader.rb
@@ -3,9 +3,19 @@ require "csv"
 module Sapristi
 	class ConfigurationLoader
 		SEPARATOR = ","
+		def initialize
+			@monitor_manager = MonitorManager.new
+		end
+		
 		def load file
 			table = CSV.read(file, headers: true, col_sep: SEPARATOR)
-			raise Error, "Invalid configuration file: headers=#{table.headers.join(', ')}, valid=#{valid_headers.join(', ')}"
+			raise Error, "Invalid configuration file: headers=#{table.headers.join(', ')}, valid=#{valid_headers.join(', ')}" if !table.headers.eql? valid_headers
+
+			table.each_with_index do |definition, index|
+				definition["Line"] = index
+			end
+
+			table.map{|definition| normalize(definition) }
 		rescue Errno::ENOENT
 			raise Error, "Configuration file not found: #{file}"
 		end
@@ -13,6 +23,30 @@ module Sapristi
 		private
 		def valid_headers
 			%w(Title Command Monitor X-position Y-position H-size V-size Workspace)
+		end
+
+		def normalize definition
+			monitor = @monitor_manager.get_monitor definition["monitor"]
+
+			translations = { "H-size" => "x", "V-size" => "y", "X-position" => "x", "Y-position" => "y" }
+			normalized = definition.to_h.keys.each_with_object({}) do |k, memo|
+				m = definition[k]&.to_s&.match(/^([0-9]{1,2})%$/)
+				m2 = definition[k]&.to_s&.match(/^([0-9]+)$/)
+				if m
+					if translations[k]
+						memo[k] = (monitor[translations[k]] * (m[1].to_i / 100.0)).to_i
+					else
+						raise "#{k}=#{definition[k]}, using percentage in invalid field, valid=#{translations.keys.join(', ')}"
+					end
+				elsif m2
+					memo[k] = definition[k].to_i
+				else
+					memo[k] = definition[k]
+				end
+			end
+
+			translations.keys.each {|k| normalized[k] = normalized[k].to_i }
+			normalized
 		end
 	end
 end
--- spec/configuration_loader_spec.rb
+++ spec/configuration_loader_spec.rb
@@ -10,12 +10,35 @@ RSpec.describe ConfigurationLoader do
 		"/tmp/non_existent_file_path.csv"
 	end
 
-	let(:invalid_csv) {
+	let(:invalid_csv) do
 		file = Tempfile.new('foo')
 		file.write("hello world")
 		file.close
 		file.path
-	}
+	end
+
+	let(:valid_headers) { %w(Title Command Monitor X-position Y-position H-size V-size Workspace) }
+	let(:separator) { ","}
+
+	let(:valid_csv) do
+		file = Tempfile.new('bar')
+		file.write(valid_headers.join(separator))
+		file.write "\n"
+		valid_csv_definitions.each do |valid_definition|
+			file.write valid_headers.map {|header| valid_definition[header]}.join(separator)
+			file.write "\n"
+		end
+		file.close
+
+		file.path
+	end
+
+	let(:valid_csv_definitions) do
+		[
+			{"Title" => nil, "Command" => "some", "Monitor" => nil, "X-position" => 1, "Y-position" => 2, "H-size" => 3, "V-size" => 4, "Workspace" => 5, "Line" => 0},
+			{"Title" => "some title", "Command" => nil, "Monitor" => 6, "X-position" => 7, "Y-position" => 8, "H-size" => 9, "V-size" => 10, "Workspace" => 11, "Line" => 1}
+			]
+	end
 
 
 	it 'raises an error when configuration file is not found' do
@@ -24,5 +47,9 @@ RSpec.describe ConfigurationLoader do
 
 	it 'raises an error when csv format is invalid' do
 		expect { under_test.load(invalid_csv) }.to raise_error(Error, /Invalid configuration file: headers/)
+	end
+
+	it 'reads valid configuration file' do
+		expect(under_test.load(valid_csv)).to eql(valid_csv_definitions)
 	end
 end
