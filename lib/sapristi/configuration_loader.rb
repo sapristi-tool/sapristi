2b83d1de861c442be6e2635764b24aa46e47c21a
user.name <user.email>
2020-11-15 02:21:12 +0100

    Deal with invalid csv headers

--- lib/sapristi/configuration_loader.rb
+++ lib/sapristi/configuration_loader.rb
@@ -5,8 +5,14 @@ module Sapristi
 		SEPARATOR = ","
 		def load file
 			table = CSV.read(file, headers: true, col_sep: SEPARATOR)
+			raise Error, "Invalid configuration file: headers=#{table.headers.join(', ')}, valid=#{valid_headers.join(', ')}"
 		rescue Errno::ENOENT
 			raise Error, "Configuration file not found: #{file}"
+		end
+
+		private
+		def valid_headers
+			%w(Title Command Monitor X-position Y-position H-size V-size Workspace)
 		end
 	end
 end
--- spec/configuration_loader_spec.rb
+++ spec/configuration_loader_spec.rb
@@ -5,10 +5,24 @@ include Sapristi
 
 RSpec.describe ConfigurationLoader do
 	let(:under_test) { ConfigurationLoader.new }
+	
 	let(:non_existing_file) do
 		"/tmp/non_existent_file_path.csv"
 	end
+
+	let(:invalid_csv) {
+		file = Tempfile.new('foo')
+		file.write("hello world")
+		file.close
+		file.path
+	}
+
+
 	it 'raises an error when configuration file is not found' do
 		expect { under_test.load(non_existing_file) }.to raise_error(Error, /Configuration file not found: #{non_existing_file}/)
+	end
+
+	it 'raises an error when csv format is invalid' do
+		expect { under_test.load(invalid_csv) }.to raise_error(Error, /Invalid configuration file: headers/)
 	end
 end
