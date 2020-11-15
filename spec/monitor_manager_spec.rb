ec24c1872685b8b533866371c5384db348d06d5b
user.name <user.email>
2020-11-15 01:59:25 +0100

    Get another monitor

--- spec/monitor_manager_spec.rb
+++ spec/monitor_manager_spec.rb
@@ -5,11 +5,13 @@ include Sapristi
 RSpec.describe MonitorManager do
 	let(:under_test) { MonitorManager.new }
 	let(:a_monitor_name) { "DP-1" }
-	let(:a_monitor) { { id: 0, name: "DP-1", main: "*", x: 3840, y: 2160, offset_x: 0, offset_y: 0 }.transform_keys(&:to_s) }
+	let(:another_monitor_name) { "HDMI-1" }
+	let(:a_monitor) { { id: 0, name: a_monitor_name, main: "*", x: 3840, y: 2160, offset_x: 0, offset_y: 0 }.transform_keys(&:to_s) }
+	let(:another_monitor) { { id: 1, name: another_monitor_name, main: nil, x: 1920, y: 1080, offset_x: 3840, offset_y: 0 }.transform_keys(&:to_s) }
 	let(:xrandr_example) {
 %Q{Monitors: 2
- 0: +*DP-1 3840/597x2160/336+0+0  DP-1
- 1: +HDMI-1 1920/509x1080/286+3840+0  HDMI-1
+ 0: +*#{a_monitor_name} 3840/597x2160/336+0+0  DP-1
+ 1: +#{another_monitor_name} 1920/509x1080/286+3840+0  HDMI-1
 }
 	}
 
@@ -23,5 +25,11 @@ RSpec.describe MonitorManager do
 		expect(under_test).to receive(:list_monitors).and_return(xrandr_example)
 
 		expect(under_test.get_monitor(a_monitor_name)).to eql(a_monitor)
+	end
+
+	it 'get another monitor' do
+		expect(under_test).to receive(:list_monitors).and_return(xrandr_example)
+
+		expect(under_test.get_monitor(another_monitor_name)).to eql(another_monitor)
 	end
 end
