1d9dad977ad270810b1499e00779e31c722e4dba
user.name <user.email>
2020-11-15 01:56:08 +0100

    Get monitor by name

--- lib/sapristi/monitor_manager.rb
+++ lib/sapristi/monitor_manager.rb
@@ -1,9 +1,21 @@
 module Sapristi
 	class MonitorManager
 		def get_monitor name
-			`axrandr --listmonitors`
+			monitors[name]
+		end
+
+		private
+		def monitors
+			list_monitors.split("\n")[1..nil]
+				.map {|line| line.match(/^\s*+(?<id>[0-9]+):\s*\+(?<main>\*)?(?<name>[^\s]+)\s+(?<x>[0-9]+)\/[0-9]+x(?<y>[0-9]+)\/[0-9]+\+(?<offset_x>[0-9]+)\+(?<offset_y>[0-9]+).*$/)}
+				.map {|m| m.names.each_with_object({}) {|name, memo| memo[name] = m[name]&.match(/^[0-9]+$/) ? m[name].to_i : m[name] }}
+				.each_with_object({}) {|monitor, memo| memo[monitor["name"]] = monitor }
 		rescue StandardError => e
 			raise Error.new "Error fetching monitor information: #{e}"
+		end
+
+		def list_monitors
+			`xrandr --listmonitors`
 		end
 	end
 end
--- spec/monitor_manager_spec.rb
+++ spec/monitor_manager_spec.rb
@@ -4,8 +4,24 @@ include Sapristi
 
 RSpec.describe MonitorManager do
 	let(:under_test) { MonitorManager.new }
+	let(:a_monitor_name) { "DP-1" }
+	let(:a_monitor) { { id: 0, name: "DP-1", main: "*", x: 3840, y: 2160, offset_x: 0, offset_y: 0 }.transform_keys(&:to_s) }
+	let(:xrandr_example) {
+%Q{Monitors: 2
+ 0: +*DP-1 3840/597x2160/336+0+0  DP-1
+ 1: +HDMI-1 1920/509x1080/286+3840+0  HDMI-1
+}
+	}
 
 	it 'raises an error if xrandr execution fails' do
+		expect(under_test).to receive(:list_monitors).and_wrap_original {|m, *args| `axrandr --listmonitors` }
+
 		expect { under_test.get_monitor(nil) }.to raise_error(Error, /Error fetching monitor information/)
+	end
+
+	it 'get a monitor' do
+		expect(under_test).to receive(:list_monitors).and_return(xrandr_example)
+
+		expect(under_test.get_monitor(a_monitor_name)).to eql(a_monitor)
 	end
 end
