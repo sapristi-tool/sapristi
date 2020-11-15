735b3a99ab7d193bc8a68d92d4fa3bdfe7ab5556
user.name <user.email>
2020-11-15 01:27:46 +0100

    Remove scafold test

--- spec/sapristi_spec.rb
+++ spec/sapristi_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Sapristi do
   it "has a version number" do
     expect(Sapristi::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
