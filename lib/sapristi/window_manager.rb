898524cd3a5e50bf39235eb644544671382f16ff
user.name <user.email>
2020-11-16 14:16:49 +0100

    Add launching capabilities

--- lib/sapristi/window_manager.rb
+++ lib/sapristi/window_manager.rb
@@ -9,5 +9,42 @@ module Sapristi
 		def windows
 			@display.windows
 		end
+
+		def launch cmd, timeout_in_seconds = 30
+			windows = @display.windows
+			windows_data = windows.map {|w| w.to_h }
+
+			user_id = `id -u`.strip
+			previous_pids = `ps -u #{user_id}`.split("\n")[1..nil].map(&:to_i)
+
+			process_pid = Process.spawn(cmd) rescue (raise Error, "Error executing process: #{$!.to_s}")
+			puts "Launch #{cmd.split[0]}, process=#{process_pid}"
+			waiter = Process.detach process_pid
+
+			
+			start_time = Time.now
+			while Time.now - start_time < timeout_in_seconds && waiter.alive?
+				new_windows_found = @display.windows.filter {|w| !windows_data.include? w.to_h }.filter {|w| !previous_pids.include? w.pid}
+
+				new_windows_found.each {|a| puts "  Found new window=#{a.pid}, process=#{process_pid}: #{a.title}"}
+				process_window = new_windows_found.find {|window| window.pid.eql? process_pid }
+				
+				break if process_window
+				
+				sleep 0.5
+			end
+
+			if !waiter.alive?
+				raise Error, "Error executing process, is dead"
+			end
+
+			if process_window.nil?
+				Process.kill"KILL", process_pid
+				raise Error, "Error executing process, it didn't open a window"
+			end
+
+			puts "Found window title=#{process_window.title} for process=#{process_pid}!"
+			process_window
+		end
 	end
 end
--- spec/window_manager_spec.rb
+++ spec/window_manager_spec.rb
@@ -12,4 +12,39 @@ RSpec.describe WindowManager do
 		
 		expect(actual).to contain_exactly(*expected)
 	end
+
+	it ('can resize and move window') do
+		pid = Process.spawn("gedit --new-window")
+		Process.detach pid
+
+		under_test.windows.find {}
+	ensure
+		Process.kill "KILL", pid
+	end
+
+	context('execute and return window') do
+		it('raises and error when command is invalid') do
+			expect { under_test.launch("invalid_command") }.to raise_error(Error, /Error executing process: No such file or directory/)
+		end
+
+		it('raises and error when command ends') do
+			expect { under_test.launch("/bin/ls > /dev/null") }.to raise_error(Error, /Error executing process, is dead/)
+		end
+
+		it('raises and error when command does not create a window') do
+			expect { under_test.launch("bash -c \"read\"", 1) }.to raise_error(Error, /Error executing process, it didn't open a window/)
+		end
+
+		it ('launches a new gedit window and process') do
+			user_id = `id -u`.strip
+			previous_pids = `ps -u #{user_id}`.split("\n")[1..nil].map(&:to_i)
+
+			window = under_test.launch("gedit --new-window /tmp/some_file.txt")
+			expect(previous_pids).not_to  include(window.pid)
+		ensure
+			Process.kill "KILL", window.pid if window&.pid
+		end
+
+
+	end
 end
