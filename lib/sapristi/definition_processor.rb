86190f76e42c30c74e816fbbb34c3640d02dfa40
user.name <user.email>
2020-11-17 21:56:44 +0100

    Definition processor

--- lib/sapristi/definition_processor.rb
+++ lib/sapristi/definition_processor.rb
@@ -1,7 +1,23 @@
 module Sapristi
 	class DefinitionProcessor
-	end
+		def initialize window_manager = WindowManager.new
+			@window_manager = window_manager
+		end
 
-	def process_definition definition
+		def process_definition definition
+			if definition["Title"]
+				windows = @window_manager.find_window(/#{definition["Title"]}/)
+				raise Error, "#{windows.size} windows have the same title: #{definition['Title']}" if windows.size > 1
+
+				window = windows[0]
+			end
+
+			if window.nil? && definition["Command"]
+				window = @window_manager.launch definition["Command"]
+			end
+
+			raise Error, "Couldn't produce a window for this definition" if !window
+			window
+		end
 	end	
 end
---
+++ spec/definition_processor_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+include Sapristi
+
+RSpec.describe DefinitionProcessor do
+
+	it('launches command when window title not specified') do
+		window_manager = spy("window_manager")
+		command = "sol"
+		DefinitionProcessor.new(window_manager).process_definition({ "Command" => command })
+
+		expect(window_manager).to have_received(:launch).with(command)
+	end
+
+	it('throws an error if window with title is not present and command not supplied') do
+		
+		expect do
+			DefinitionProcessor.new().process_definition({ "Title" => "non existing window title", "Command" => nil })
+		end.to raise_error Error, "Couldn't produce a window for this definition"
+	end
+
+	it('throws an error if more than one window have the same title') do
+		window_manager = WindowManager.new
+		window1 = window_manager.launch "sol"
+		window2 = window_manager.launch "sol"
+
+		duplicated_title = /Klondike/
+		expect do
+			DefinitionProcessor.new().process_definition({ "Title" => duplicated_title, "Command" => nil })
+		end.to raise_error Error, "2 windows have the same title: #{duplicated_title}"
+	ensure
+		window_manager.close(window1) if window1
+		window_manager.close(window2) if window2
+	end
+end
