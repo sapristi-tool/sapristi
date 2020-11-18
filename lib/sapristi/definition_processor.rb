a6dce887fc6dd21ef1accc6e611d3d308294f4d5
user.name <user.email>
2020-11-18 22:58:06 +0100

    Missing bits for definition processor, move and resize

--- lib/sapristi/definition_processor.rb
+++ lib/sapristi/definition_processor.rb
@@ -7,14 +7,22 @@ module Sapristi
     end
 
     def process_definition(definition)
-      if definition['Title']
-        windows = @window_manager.find_window(/#{definition["Title"]}/)
-        raise Error, "#{windows.size} windows have the same title: #{definition['Title']}" if windows.size > 1
+      window = get_window definition['Title'], definition['Command']
+      
+      @window_manager.resize(window, definition["H-size"], definition["V-size"])
+      @window_manager.move(window, definition["X-position"], definition["Y-position"])
+    end
+
+    private
+    def get_window(title, command)
+    	if title
+        windows = @window_manager.find_window(/#{title}/)
+        raise Error, "#{windows.size} windows have the same title: #{title}" if windows.size > 1
 
         window = windows[0]
       end
 
-      window = @window_manager.launch definition['Command'] if window.nil? && definition['Command']
+      window = @window_manager.launch command if window.nil? && command
 
       raise Error, "Couldn't produce a window for this definition" unless window
 
--- spec/definition_processor_spec.rb
+++ spec/definition_processor_spec.rb
@@ -4,32 +4,66 @@ require 'spec_helper'
 
 module Sapristi
   RSpec.describe DefinitionProcessor do
-    it('launches command when window title not specified') do
-      window_manager = spy('window_manager')
-      command = 'sol'
-      DefinitionProcessor.new(window_manager).process_definition({ 'Command' => command })
+    context('fetch window') do
+      it('launches command when window title not specified') do
+        window_manager = spy('window_manager')
+        command = 'sol'
+        DefinitionProcessor.new(window_manager).process_definition({ 'Command' => command })
 
-      expect(window_manager).to have_received(:launch).with(command)
+        expect(window_manager).to have_received(:launch).with(command)
+      end
+
+      it('throws an error if window with title is not present and command not supplied') do
+        expect do
+          DefinitionProcessor.new.process_definition({ 'Title' => 'non existing window title', 'Command' => nil })
+        end.to raise_error Error, "Couldn't produce a window for this definition"
+      end
+
+      it('throws an error if more than one window have the same title') do
+        window_manager = WindowManager.new
+        window1 = window_manager.launch 'sol'
+        window2 = window_manager.launch 'sol'
+
+        duplicated_title = /Klondike/
+        expect do
+          DefinitionProcessor.new.process_definition({ 'Title' => duplicated_title, 'Command' => nil })
+        end.to raise_error Error, "2 windows have the same title: #{duplicated_title}"
+      ensure
+        window_manager.close(window1) if window1
+        window_manager.close(window2) if window2
+      end
     end
 
-    it('throws an error if window with title is not present and command not supplied') do
-      expect do
-        DefinitionProcessor.new.process_definition({ 'Title' => 'non existing window title', 'Command' => nil })
-      end.to raise_error Error, "Couldn't produce a window for this definition"
-    end
+    context 'manipulate window' do
+      let(:window) { double('window') }
+      let(:window_manager) { spy('window_manager') }
+      let(:under_test) do
+        manager = DefinitionProcessor.new(window_manager)
+        allow(manager).to receive(:get_window).and_return(window)
 
-    it('throws an error if more than one window have the same title') do
-      window_manager = WindowManager.new
-      window1 = window_manager.launch 'sol'
-      window2 = window_manager.launch 'sol'
+        manager
+      end
+      let(:x_position) { 1 }
+      let(:y_position) { 2 }
+      let(:size_x) { 3 }
+      let(:size_y) { 4 }
 
-      duplicated_title = /Klondike/
-      expect do
-        DefinitionProcessor.new.process_definition({ 'Title' => duplicated_title, 'Command' => nil })
-      end.to raise_error Error, "2 windows have the same title: #{duplicated_title}"
-    ensure
-      window_manager.close(window1) if window1
-      window_manager.close(window2) if window2
+      let(:definition) do
+        { 'Command' => 'sol', 'X-position' => x_position, 'Y-position' => y_position,
+          'H-size' => size_x, 'V-size' => size_y }
+      end
+
+      it 'move window' do
+        under_test.process_definition(definition)
+
+        expect(window_manager).to have_received(:move).with(window, x_position, y_position)
+      end
+
+      it 'move window' do
+        under_test.process_definition(definition)
+
+        expect(window_manager).to have_received(:resize).with(window, size_x, size_y)
+      end
     end
   end
 end
