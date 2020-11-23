723801a28bd6fd406f0c3c8833462f46b6481454
user.name <user.email>
2020-11-23 12:08:58 +0100

    More rubocop

--- lib/sapristi/sapristi.rb
+++ lib/sapristi/sapristi.rb
@@ -9,18 +9,11 @@ module Sapristi
 
     def run(conf_file = user_default_configuration_file)
       ::Sapristi.logger.info "Sapristi: Processing: #{conf_file}"
-      if conf_file.eql?(user_default_configuration_file) && !File.exist?(conf_file)
-        @configuration_loader.create_empty_configuration conf_file
-      end
+      check_user_configuration(conf_file)
 
       definitions = @configuration_loader.load(conf_file)
 
-      definitions.each_with_index do |definition, index|
-        ::Sapristi.logger.info "Process line #{index}: #{definition.inspect}"
-        @definition_processor.process_definition(definition) unless @dry
-      rescue Error => e
-        raise Error, "#{e.message}, line=#{index}"
-      end
+      process definitions
     end
 
     def verbose!
@@ -34,9 +27,23 @@ module Sapristi
 
     private
 
-    USER_CONFIGURATION_FILE = File.join Dir.home, '.sapristi.csv'
+    def process(definitions)
+      definitions.each_with_index do |definition, index|
+        ::Sapristi.logger.info "Process line #{index}: #{definition.inspect}"
+        @definition_processor.process_definition(definition) unless @dry
+      rescue Error => e
+        raise Error, "#{e.message}, line=#{index}"
+      end
+    end
+
+    def check_user_configuration(conf_file)
+      if conf_file.eql?(user_default_configuration_file) && !File.exist?(conf_file)
+        @configuration_loader.create_empty_configuration conf_file
+      end
+    end
+
     def user_default_configuration_file
-      USER_CONFIGURATION_FILE
+      File.join Dir.home, '.sapristi.csv'
     end
   end
 end
